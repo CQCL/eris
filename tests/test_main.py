b4d590af64b21cc766a780b651943c0f2f79930c
Seyon Sivarajah <seyon.sivarajah@quantinuum.com>
2024-12-20 11:59:55 +0000

    updates

--- tests/test_main.py
+++
@@ -1,8 +0,0 @@
-"""Tests for eris.main module."""
-
-from eris.main import hello_world
-
-
-def test_hello_world():
-    """Test the hello_world function."""
-    assert hello_world() == "Hello, World!"
