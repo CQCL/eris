a838006a1d15c8f2a00ddf494c503bc894cb6eb7
Seyon Sivarajah <seyon.sivarajah@quantinuum.com>
2024-12-23 15:10:31 +0000

    add one and ft tests

--- src/eris/steane.py
+++ src/eris/steane.py
@@ -8,24 +8,33 @@ from guppylang.std import quantum as gq
 def non_ft_zero() -> array[qubit, 7]:
     data_qubits = array(qubit() for _ in range(7))
     plus_ids = array(0, 4, 6)
-    # plus_ids = array(0, 1, 2)
     for i in plus_ids:
         gq.h(data_qubits[i])
 
     cx_pairs = array((0, 1), (4, 5), (6, 3), (6, 5), (4, 2), (0, 3), (4, 1), (3, 2))
-    # cx_pairs = array(
-    #     (6, 4),
-    #     (6, 5),
-    #     (0, 3),
-    #     (1, 3),
-    #     (0, 5),
-    #     (2, 3),
-    #     (1, 4),
-    #     (0, 6),
-    #     (2, 4),
-    #     (1, 6),
-    #     (2, 5),
-    # )
     for c, t in cx_pairs:
         gq.cx(data_qubits[c], data_qubits[t])
     return data_qubits
+
+
+@guppy
+def ft_zero() -> tuple[array[qubit, 7], bool]:
+    data_qubits = non_ft_zero()
+    # TODO compilation barrier
+    ancilla = qubit()
+    flags = array(1, 3, 5)
+    for f in flags:
+        gq.cx(data_qubits[f], ancilla)
+    return data_qubits, gq.measure(ancilla)
+
+
+@guppy
+def x(data_qubits: array[qubit, 7]) -> None:
+    for i in range(len(data_qubits)):
+        gq.x(data_qubits[i])
+
+
+@guppy
+def z(data_qubits: array[qubit, 7]) -> None:
+    for i in range(len(data_qubits)):
+        gq.z(data_qubits[i])
--- tests/test_steane_prep.py
+++ tests/test_steane_prep.py
@@ -1,7 +1,7 @@
 from guppylang.decorator import guppy
-from guppylang.std.builtins import array, result
-from guppylang.std.quantum import qubit, measure
-from eris.steane import non_ft_zero
+from guppylang.std.builtins import result
+from guppylang.std import quantum as gq
+from eris.steane import non_ft_zero, x, ft_zero
 from .util import run, single_reg_counts, even_parity
 
 
@@ -13,28 +13,69 @@ def permute_canonical(bitstr: str) -> str:
     )
 
 
+def flip_bits(bitstr: str) -> str:
+    return "".join("1" if b == "0" else "0" for b in bitstr)
+
+
+ZERO_BASES = {
+    "0000000",
+    "1010101",
+    "0110011",
+    "1100110",
+    "0001111",
+    "1011010",
+    "0111100",
+    "1101001",
+}
+
+
 def test_non_ft_zero() -> None:
     @guppy
     def main() -> None:
         data_qubits = non_ft_zero()
         for q in data_qubits:
-            result("c", measure(q))
+            result("zero", gq.measure(q))
 
-    counts = single_reg_counts(run(guppy.get_module(), 7, n_shots=100))
-    bitsts = set(counts.keys())
+        data_qubits = non_ft_zero()
+        x(data_qubits)
+        for q in data_qubits:
+            result("one", gq.measure(q))
+
+    results = run(guppy.get_module(), 7, n_shots=100)
+    bitsts = set(single_reg_counts(results, "zero").keys())
     assert len(bitsts) == 8
     assert all(even_parity(bitstr) for bitstr in bitsts)
 
     # permute to canonical labelling of data qubits
     # https://errorcorrectionzoo.org/c/steane
     bitsts = {permute_canonical(bitstr) for bitstr in bitsts}
-    assert bitsts == {
-        "0000000",
-        "1010101",
-        "0110011",
-        "1100110",
-        "0001111",
-        "1011010",
-        "0111100",
-        "1101001",
-    }
+
+    assert bitsts == ZERO_BASES
+
+    # test X gate
+    bitsts = set(single_reg_counts(results, "one").keys())
+    assert len(bitsts) == 8
+    assert all(not even_parity(bitstr) for bitstr in bitsts)
+    one_bases = {flip_bits(b) for b in ZERO_BASES}
+    assert {permute_canonical(bitstr) for bitstr in bitsts} == one_bases
+
+
+def test_ft_zero() -> None:
+    @guppy
+    def main() -> None:
+        data_qubits, goto = ft_zero()
+        for q in data_qubits:
+            result("ft_zero", gq.measure(q))
+
+        result("goto", goto)
+
+    results = run(guppy.get_module(), 8, n_shots=100)
+    bitsts = set(single_reg_counts(results, "ft_zero").keys())
+    assert len(bitsts) == 8
+    assert all(even_parity(bitstr) for bitstr in bitsts)
+
+    bitsts = {permute_canonical(bitstr) for bitstr in bitsts}
+
+    assert bitsts == ZERO_BASES
+    # TODO test with error model
+    assert single_reg_counts(results, "goto") == {"0": 100}
