ddf75b9bdc9d4a0236945d25f61843062ee9efb8
Seyon Sivarajah <seyon.sivarajah@quantinuum.com>
2024-12-23 10:45:00 +0000

    decorator tests

---
+++ src/eris/decorator.py
@@ -0,0 +1,27 @@
+from inspect import isclass
+from typing import TypeVar, Generic
+
+
+T = TypeVar("T", bound=type)
+
+    
+
+def block(cls: T) -> T:
+    cls.blocked = True
+    if isclass(cls):
+        print("class!")
+    else:
+        print("method!")
+    return cls
+
+
+# Example usage
+@block
+class MyClass:
+    @block
+    def hello(self):
+        print("Hello")
+
+
+print(MyClass.blocked)  # Output: True
+print(MyClass.hello.blocked)  # Output: True
