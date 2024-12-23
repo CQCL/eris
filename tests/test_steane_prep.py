c4858c5d8056077f0d59a4ebdafaecb01b6cecbf
Seyon Sivarajah <seyon.sivarajah@quantinuum.com>
2024-12-23 14:13:09 +0000

    test basis states

--- tests/test_steane_prep.py
+++ tests/test_steane_prep.py
@@ -5,6 +5,14 @@ from eris.steane import non_ft_zero
 from .util import run, single_reg_counts, even_parity
 
 
+def permute_canonical(bitstr: str) -> str:
+    map_from_canonical = {3: 0, 5: 1, 6: 2, 4: 3, 1: 4, 2: 5, 0: 6}
+
+    return "".join(
+        bitstr[map_from_canonical[i]] for i in range(len(map_from_canonical))
+    )
+
+
 def test_non_ft_zero() -> None:
     @guppy
     def main() -> None:
@@ -17,14 +25,16 @@ def test_non_ft_zero() -> None:
     assert len(bitsts) == 8
     assert all(even_parity(bitstr) for bitstr in bitsts)
 
-
-    # assert bitsts == {
-    #     "0000000",
-    #     "1010101",
-    #     "0110011",
-    #     "1100110",
-    #     "0001111",
-    #     "1011010",
-    #     "0111100",
-    #     "1101001",
-    # }
+    # permute to canonical labelling of data qubits
+    # https://errorcorrectionzoo.org/c/steane
+    bitsts = {permute_canonical(bitstr) for bitstr in bitsts}
+    assert bitsts == {
+        "0000000",
+        "1010101",
+        "0110011",
+        "1100110",
+        "0001111",
+        "1011010",
+        "0111100",
+        "1101001",
+    }
